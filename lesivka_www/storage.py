a493fa1014fd538d99d0261ec30c92501cb25dce
Mystic-Mirage <mm@m10e.net>
2019-09-16 20:10:03 +0300

    Move ndb.Client() to module level

--- lesivka_www/storage.py
+++ lesivka_www/storage.py
@@ -1,6 +1,7 @@
 from google.cloud import ndb
 
 NOT_SET = 'NOT SET'
+client = ndb.Client()
 
 
 class Settings(ndb.Model):
@@ -9,8 +10,7 @@ class Settings(ndb.Model):
 
     @classmethod
     def get(cls, name):
-
-        with ndb.Client().context():
+        with client.context():
             entity = cls.query(Settings.name == name).get()
 
             if entity is None:
@@ -23,5 +23,3 @@ class Settings(ndb.Model):
             raise ValueError(name)
 
         return entity.value
-
-

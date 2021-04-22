35795748a26982cc01371942854ff29acc742f7d
Mystic-Mirage <mm@m10e.net>
2021-04-23 02:12:53 +0300

    Allow add the bot to groups

--- lesivka_www/routes/bot.py
+++ lesivka_www/routes/bot.py
@@ -10,6 +10,15 @@ bot = Blueprint("bot_hook", __name__)
 token = os.environ["TELEGRAM_BOT_TOKEN"]
 telebot = TeleBot(token)
 
+START_MSG = "Конвертер з кирилиці у лесівку. Подробиці https://lesivka.com"
+
+
+def chat_type(*types):
+    def f(message):
+        return message.chat.type in types
+
+    return f
+
 
 @bot.route(f"/{token}", methods=["POST"])
 def hook():
@@ -18,14 +27,24 @@ def hook():
     return Response(status=200)
 
 
-@telebot.message_handler(commands=["start"])
+@telebot.message_handler(commands=["start"], func=chat_type("private"))
 def start(message: Message):
-    telebot.send_message(
-        message.chat.id,
-        "Конвертер з кирилиці у лесівку. Подробиці https://lesivka.com",
-    )
+    telebot.send_message(message.chat.id, START_MSG)
 
 
-@telebot.message_handler(func=lambda m: True)
+@telebot.message_handler(
+    commands=["lesivka"],
+    func=chat_type("group", "supergroup"),
+)
+def convert(message: Message):
+    try:
+        _, text = message.text.split(maxsplit=1)
+    except ValueError:
+        telebot.send_message(message.chat.id, START_MSG)
+    else:
+        telebot.reply_to(message, lesivka.encode(text))
+
+
+@telebot.message_handler(func=chat_type("private"))
 def encode(message: Message):
     telebot.reply_to(message, lesivka.encode(message.text))

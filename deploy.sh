067ac8d250b93484187519917446576fcfb0f12a
Mystic-Mirage <mm@m10e.net>
2023-08-31 10:21:18 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 gcloud app deploy app.yaml --version current
 # shellcheck disable=SC2046
-gcloud storage rm -r $(gcloud storage buckets list --uri | rev | cut -d / -f 1 | rev | sed 's|^|gs://|')
+gcloud storage rm -r $(gcloud storage buckets list | grep storage_url | cut -d " " -f 2)

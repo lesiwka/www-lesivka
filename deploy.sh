627364d3cf885d4ef3e4c19aedd6e6d774dba7e7
Mystic-Mirage <mm@m10e.net>
2023-09-03 16:02:37 +0300

    Repair the app before deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+gcloud beta app repair
 gcloud app deploy app.yaml --version current
 # shellcheck disable=SC2046
 gcloud storage rm -r $(gcloud storage buckets list | grep storage_url | cut -d " " -f 2)

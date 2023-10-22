f9727b527e6102598657e85826339b7b8c535e8e
Mystic-Mirage <mm@m10e.net>
2023-10-22 15:05:07 +0300

    Do deploy with --no-cache

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 gcloud config get project | grep 'lesi[vw]ka' | grep -E 'web|www' || { echo ERROR: WRONG PROJECT; exit 1; }
 
 gcloud beta app repair
-gcloud app deploy app.yaml --version current
+gcloud app deploy app.yaml --version current --no-cache
 # shellcheck disable=SC2046
 gcloud storage rm -r $(gcloud storage buckets list | grep storage_url | cut -d " " -f 2)

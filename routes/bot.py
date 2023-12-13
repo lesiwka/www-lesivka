5d5299dfce679fbdd4c44beffcdcecc836cdcc5f
Mystic-Mirage <mm@m10e.net>
2023-12-13 03:04:12 +0200

    Add Viber bot

--- routes/bot.py
+++ routes/bot.py
@@ -4,13 +4,24 @@ import lesiwka
 from flask import Blueprint, Response, request
 from telebot import TeleBot
 from telebot.types import Message, Update
+from viberbot import Api, BotConfiguration
+from viberbot.api.messages import TextMessage
+from viberbot.api.viber_requests import ViberMessageRequest, ViberSubscribedRequest
 
 bot = Blueprint("bot_hook", __name__)
 
 token = os.environ["TELEGRAM_BOT_TOKEN"]
 telebot = TeleBot(token)
 
-START_MSG = "Конвертер з кирилиці у лесівку. Подробиці https://lesiwka.com"
+viber_token = os.environ["VIBER_BOT_TOKEN"]
+viber_conf = BotConfiguration(
+    viber_token,
+    "Lesiwka Bot",
+    "https://lesiwka.com/static/icons/android-chrome-512x512.png",
+)
+viber = Api(viber_conf)
+
+START_MSG = "Конвертер з кирилиці в лесівку. Подробиці https://lesiwka.com"
 
 
 def chat_type(*types):
@@ -24,6 +35,25 @@ def chat_type(*types):
 def hook():
     update = Update.de_json(request.json)
     telebot.process_new_updates([update])
+    return Response(status=200)
+
+
+@bot.route(f"/{viber_token}", methods=["POST"])
+def viber_hook():
+    data = request.get_data()
+    if not viber.verify_signature(
+        data, request.headers.get("X-Viber-Content-Signature")
+    ):
+        return Response(status=403)
+
+    viber_request = viber.parse_request(data)
+    if isinstance(viber_request, ViberSubscribedRequest):
+        message = TextMessage(text=START_MSG)
+        viber.send_messages(viber_request.user.id, [message])
+    elif isinstance(viber_request, ViberMessageRequest):
+        message = TextMessage(text=lesiwka.encode(viber_request.message.text))
+        viber.send_messages(viber_request.sender.id, [message])
+
     return Response(status=200)
 
 
